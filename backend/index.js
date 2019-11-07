6e6826f5f05db67fec0669b19cc532cff83fc7e2
Patricia Felix <patriciafelixx@outlook.com>
2019-11-07 20:44:39 -0300

    Add middlewares.js & cosign in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const consign = require('consign');
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app);
 
 app.listen(3000, () => {
     console.log('Backend executando...'); 
