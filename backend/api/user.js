88082368f0c27e210ce92c36defc111ce4689fd8
Patricia Felix <patriciafelixx@outlook.com>
2019-11-09 17:58:20 -0300

    Desafio getById

--- backend/api/user.js
+++ backend/api/user.js
@@ -50,5 +50,13 @@ module.exports = app => {
             .catch(err => status(500).send(err));
     }
 
-    return { save, get };
+    const getById = (req, res) => {
+        app.db('users')
+        .select('id', 'name', 'email', 'admin')
+        .where({ id: req.params.id}).first()
+        .then(user => res.json(user))
+        .catch(err => status(500).send(err));
+    }
+
+    return { save, get, getById};
 }
