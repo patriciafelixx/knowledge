f49ef6fb57987533e40892f45abe7a66cff70348
Patricia Felix <patriciafelixx@outlook.com>
2019-11-15 10:58:43 -0300

    Api de categoria

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,8 +2,17 @@ module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
-        
-        app.route('/users/:id')
+
+    app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById);
+    
+        app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    app.route('/categories/:id')
+        .put(app.api.category.save)
+        .get(app.api.category.getById)
+        .delete(app.api.category.remove);  
 }
