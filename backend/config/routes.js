9ea5d91a7ee516974d75d8aea79801d32bee2e9d
Patricia Felix <patriciafelixx@outlook.com>
2019-11-15 11:10:22 -0300

    Estruturando árvore de categorias

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -6,13 +6,17 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById);
-    
-        app.route('/categories')
+
+    app.route('/categories')
         .post(app.api.category.save)
         .get(app.api.category.get)
+
+    // importante: a rota getTree deve estar antes de /categories/:id
+    app.route('/categories/tree')
+        .get(app.api.category.getTree);
 
     app.route('/categories/:id')
         .put(app.api.category.save)
         .get(app.api.category.getById)
-        .delete(app.api.category.remove);  
+        .delete(app.api.category.remove);
 }
