74de0c1ca49fb20c8f77cf6c2ba0c3de8540da06
Patricia Felix <patriciafelixx@outlook.com>
2019-11-26 20:59:50 -0300

    Componente Menu Usuário

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import 'font-awesome/css/font-awesome.css';
 import Vue from 'vue';
 import App from './App.vue';
+import './config/bootstrap';
 import store from './config/store';
 
 Vue.config.productionTip = false;
