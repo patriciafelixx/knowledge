9e262cf6290adad2b8423d7d8457b440effc19c7
Patricia Felix <patriciafelixx@outlook.com>
2019-12-04 16:50:45 -0300

    Tela de Login e Cadastro

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,9 +8,6 @@ import router from './config/router';
 
 Vue.config.productionTip = false;
 
-// TEMPORARY AUTHORIZATION
-require('axios').defaults.headers.common['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlBhdHJpY2lhIEZlbGl4IiwiZW1haWwiOiJwYXRyaWNpYWZlbGl4eEBvdXRsb29rLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE1NzQ4NjcyMDAsImV4cCI6MTU3NTEyNjQwMH0.F-jFbuxttM02zsWu7z8QqHHgWkmA_lPVP52G-6Xgvi8'
-
 new Vue({
   store,
   router,
