b5f8ad08aaa77d7876c2bba13f355be04b42f061
Patricia Felix <patriciafelixx@outlook.com>
2019-12-05 12:49:44 -0300

    Responsividade do Menu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,13 @@
 import 'font-awesome/css/font-awesome.css';
 import Vue from 'vue';
+
 import App from './App.vue';
+
 import './config/bootstrap';
 import './config/msg';
 import './config/axios';
+import './config/mq';
+
 import store from './config/store';
 import router from './config/router';
 
